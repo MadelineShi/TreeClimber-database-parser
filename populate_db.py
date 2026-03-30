4e8f64ecbdece6b8f7bcce147f12e1ea31427108
Madeline Shi <mashi@davidson.edu>
2026-03-29 21:20:47 -0400

    Initial commit: database parser

---
+++ populate_db.py
@@ -0,0 +1,64 @@
+import json
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = conn.cursor()
+
+cursor.execute("DELETE FROM Meetings")
+cursor.execute("DELETE FROM Courses")
+
+with open("test.json") as f:
+    data = json.load(f)
+
+for course in data:
+
+    course_values = (
+        course["crn"],
+        course["course_code"],
+        course["department"],
+        course["course_title"],
+        course["grad_requirements"],
+        course["enrollment"]["current"],
+        course["enrollment"]["max"],
+        course["enrollment"]["remaining"]
+    )
+
+    cursor.execute("""
+        INSERT INTO Courses
+        (crn, course_code, department, course_title, grad_requirements, current, max, remaining)
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+    """, course_values)
+
+    for meeting in course["meetings"]:
+        meeting_values = (
+            course["crn"],
+            meeting["meeting_type"],
+            meeting["weekdays"],
+            meeting["start_time"],
+            meeting["end_time"],
+            meeting["class_time"],
+            meeting["room"]
+        )
+
+        cursor.execute("""
+            INSERT INTO Meetings
+            (crn, meeting_type, weekdays, start_time, end_time, class_time, room)
+            VALUES (%s,%s,%s,%s,%s,%s,%s)
+        """, meeting_values)
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print("Database successfully populated.")
